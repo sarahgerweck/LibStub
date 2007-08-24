2195652c1eb7ff7195b36ba441b0ae70b8254657
kaelten <kaelten@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-24 05:10:34 +0000

    LibStub: LS-4 -Basic self update framework -First draft of NewLibrary
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@18 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -1,1 +1,32 @@
- 
+--$ID$
+local LIBSTUB_MAJOR = "LibStub"
+local LIBSTUB_MINOR = "$Revision$"
+
+local _G = getfenv()
+local LibStub = _G[LIBSTUB_MAJOR]
+if LibStub and LibStub._minor < LIBSTUB_MINOR then
+   -- Upgrading
+else
+   -- First load
+   LibStub = {
+      libs = {}
+   }
+   _G[LIBSTUB_MAJOR] = LibStub
+end
+
+function LibStub:NewLibrary(major, minor)
+   minor = assert(tonumber(minor:match("%d+")), "MinorVersion must contain a numeric.") 
+    
+   local slot = self.libs[major] or {}
+   
+   if slot.minor and slot.minor > minor then 
+      return nil 
+   end
+   
+   self.libs[major] = slot
+   
+   slot.minor = minor
+   slot.instance = slot.instance or {}
+   
+   return slot.instance
+end
