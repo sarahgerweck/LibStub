60a4b24fd1d79808bc755a4f62c1f129b353e03e
cladhaire <cladhaire@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-24 13:03:07 +0000

    LibStub:  * Minor change to error checking in :NewLibrary()  * Converted space indentation to tabs for consistency
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@21 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -5,42 +5,49 @@ local LIBSTUB_MINOR = "$Revision$"
 local _G = getfenv()
 local LibStub = _G[LIBSTUB_MAJOR]
 if LibStub and LibStub._minor < LIBSTUB_MINOR then
-   -- Upgrading
+	-- Upgrading
 else
-   -- First load
-   LibStub = {
-      libs = {}
-   }
-   _G[LIBSTUB_MAJOR] = LibStub
+	-- First load
+	LibStub = {
+		libs = {}
+	}
+	_G[LIBSTUB_MAJOR] = LibStub
 end
 
 function LibStub:NewLibrary(major, minor)
-   minor = assert(tonumber(minor:match("%d+")), "MinorVersion must contain a numeric.") 
-    
-   local slot = self.libs[major] or {}
-   
-   if slot.minor and slot.minor >= minor then 
-      return nil 
-   end
-   
-   self.libs[major] = slot
-   
-   slot.minor = minor
-   slot.instance = slot.instance or {}
-   
-   return slot.instance
+	if type(minor) == "string" then
+		-- Convert revision strings into numbers
+		minor = tonumber(minor:match("%d+"))
+	end
+
+	if type(minor) ~= "number" then
+		error("Minor version must contain a number")
+	end
+	
+	local entry = self.libs[major] or {}
+	
+	if entry.minor and entry.minor >= minor then 
+		return nil 
+	end
+	
+	self.libs[major] = entry
+	
+	entry.minor = minor
+	entry.instance = entry.instance or {}
+	
+	return entry.instance
 end
 
 function LibStub:GetInstance(major)
-   if type(major) ~= "string" then
-      error(("Bad argument #2 to 'GetInstance' (string expected, got %s)"):format(type(major)), 2)
-   end
-   
-   local slot = self.libs[major]
-   
-   if not slot then
-      error(("Cannot find a library instance of %s."):format(major), 2)
-   end
-   
-   return slot.instance
+	if type(major) ~= "string" then
+		error(("Bad argument #2 to 'GetInstance' (string expected, got %s)"):format(type(major)), 2)
+	end
+	
+	local entry = self.libs[major]
+	
+	if not entry then
+		error(("Cannot find a library instance of %s."):format(major), 2)
+	end
+	
+	return entry.instance
 end
