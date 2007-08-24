4436380f12d63c5e2a9068019b338996b1c4e678
cladhaire <cladhaire@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-24 15:06:13 +0000

    LibStub:  * Added some comments to clarify intent/API  * Clarified the manual creation/upgrade process  * Added safecall()  * Fixed error checking in :FinalizeLibrary()
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@29 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -5,24 +5,21 @@ local LIBSTUB_MINOR = 1
 local _G = getfenv()
 local LibStub = _G[LIBSTUB_MAJOR]
 
--- same or older version, just leave
+-- Check to see is this version of the library is obsolete
+-- If so, return immediate without taking further action
 if LibStub and LibStub._minor >= LIBSTUB_MINOR then return end
 
--- check for upgrading
+-- If this is the first LibStub being instantiated, create it
 if not LibStub then
-	-- First load
 	LibStub = {
 		libs = {},
-		_minor = LIBSTUB_MINOR,
 	}
-	
-	_G[LIBSTUB_MAJOR] = LibStub
-else
-	-- Upgrading
-	LibStub._minor = LIBSTUB_MINOR
 end
 
--- make sure we dont overwrite anything if we updated LibStub
+-- Begin library implementation
+LibStub.minor = LIBSTUB_MINOR
+
+-- Get the metatable from LibStub, if one is already set
 local LibStub_mt = getmetatable(LibStub) or {}
 setmetatable(LibStub, LibStub_mt)
 
@@ -64,20 +61,31 @@ function LibStub:GetInstance(major)
 	return entry.instance
 end
 
+-- Set up the metatable to allow LibStub("MajorVersion")
 LibStub_mt.__call = LibStub.GetInstance
 
+local function safecall(func,...)
+    local success, err = pcall(func,...)
+    if not success then geterrorhandler()(err:find("%.lua:%d+:") and err or (debugstack():match("\n(.-: )in.-\n") or "") .. err) end
+end
+
+-- LibStub:FinalizeLibrary(major, exports, callback)
+--
+-- major (string) - The major version of the library 
+-- exports (table or function) - A table of functions to be exported into
+--   a namespace when LibStub:Embed(namespace, "Major Version") is called.
+--   If a function is supplied, then that will be called on LibStub:Embed()
+-- callback (function)  - A function to be called when a new library is loaded.
+--   If this function returns a true value, then after being called, it will
+--   be removed from the callback registry.
+--
 function LibStub:FinalizeLibrary(major, exports, callback )
 	if type(major) ~= "string" then
 		error(("Bad argument #2 to 'FinalizeLibrary' (string expected, got %s)"):format(type(major)), 2)
 	end
 
-	if type(exports) == "function" then
-		callback = exports
-		exports = nil
-	end
-
-	if type(exports) ~= "table" and type(exports) ~= "nil" then
-		error(("Bad argument #3 to 'FinalizeLibrary' (function, table or nil expected, got %s)"):format(type(exports)), 2)
+	if type(exports) ~= "table" and type(exports) ~= "function" and type(exports) ~= "nil" then
+		error(("Bad argument #3 to 'FinalizeLibrary' (string, table, nil expected, got %s)"):format(type(exports)), 2)
 	end
 
 	if type(callback) ~= "function" and type(callback) ~= "nil" then
@@ -87,19 +95,22 @@ function LibStub:FinalizeLibrary(major, exports, callback )
 	local entry = self.libs[major]
 	
 	if not entry then
-		error(("Cannot find a library instance of %s."):format(major), 2)
+		error(("Cannot finalize an unregistered instance of  %s."):format(major), 2)
 	end
 
 	-- TODO: upgrade old namespaces that have been embedded
 
-	-- store current exports and callback
+	-- Store the exports table/function and the callback function in the registry
 	entry.exports = exports
 	entry.callback = callback
 
-	-- shout out to all callbacks
-	for k, lib in pairs( self.libs ) do
-		if lib ~= entry and type(lib.callback) == "function" and lib.callback(major, entry.instance) then
-			-- some true value was received from the callback, unregister it
+	-- Iterate through all libraries, and call any callback functions
+	for key, lib in pairs(self.libs) do
+		-- Don't trigger the callback of the registering library
+		if lib ~= entry and type(lib.callback) == "function" then
+			local unregister = safecall(lib.callback, major, entry.instance)
+			
+			-- If the callback returns a true value, unregister it
 			lib.callback = nil
 		end
 	end
