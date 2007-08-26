c35a0389373f11827b3991a25632b1e1cbf63f41
kaelten <kaelten@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-26 07:40:54 +0000

    LibStub: LS-14 LS-11 LS-12 LS-13 LS-15 - Drop of callback support - Reworks and tweaks, line count at 50 - Dropped some of the error messages. - Split up internal registry
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@47 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -1,15 +1,14 @@
 -- $Id$
-local _G = getfenv()
+-- LicenseText
 local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibStub", 1
 local LibStub = _G[LIBSTUB_MAJOR]
 
 -- Check to see is this version of the library is obsolete
 if not LibStub or LibStub.minor < LIBSTUB_MINOR then 
 	if not LibStub then 
-		LibStub = {libs = {},}
-		setmetatable(LibStub, {})
-		_G[LIBSTUB_MAJOR] = LibStub
-	 end
+        LibStub = {libs = {}, minors = {}, } 
+        _G[LIBSTUB_MAJOR] = LibStub
+    end
 
 	-- LibStub:NewLibrary(major, minor)
 	-- major (string) - the major version of the library
@@ -18,16 +17,14 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- returns nil if a newer or same version of the lib is already present
 	-- returns empty library object or old library object if upgrade is needed
 	function LibStub:NewLibrary(major, minor)
-		minor = tonumber(minor) or assert(tonumber(minor:match("%d+")), "Minor version must either be a number or contain a number.")
-	
-		local entry = self.libs[major] or {}
-	
-		if entry.minor and entry.minor >= minor then return nil end
-	
-		self.libs[major] = entry
-		entry.minor, entry.instance = minor, entry.instance or {}
-	
-		return entry.instance
+        assert(type(major) == "string", "Bad argument #2 to 'NewLibrary' (string expected)")
+        assert(minor, "Minor version must either be a number or contain a number.")
+        minor = tonumber(minor) or assert(tonumber(minor:match("%d+")), "Minor version must either be a number or contain a number.")
+        
+        if self.minors[major] and self.minors[major] >= minor then return nil end
+        self.minors[major], self.libs[major] = minor, self.libs[major] or {}
+        
+		return self.libs[major]
 	end
 
 	-- LibStub:GetLibrary(major, [silent])
@@ -37,46 +34,17 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- throws an error if the library can not be found
 	-- returns the library object if found
 	function LibStub:GetLibrary(major, silent)
-		assert(type(major) == "string", ("Bad argument #2 to 'GetLibrary' (string expected, got %s)"):format(type(major)))
-	
-		local entry = silent and self.libs[major] or assert(self.libs[major], ("Cannot find a library instance of %s."):format(major))
-	
-		return entry and entry.instance or nil
-	end
+        assert(type(major) == "string", "Bad argument #2 to 'GetLibrary' (string expected)")
+		return not silent and assert(self.libs[major], "Library instance not found.") or self.libs[major]
+    end
 
-	local function safecall(func,...)
-		local success, err = pcall(func,...)
-		if success then return err end
-		
-		if not err:find("%.lua:%d+:") then err = (debugstack():match("\n(.-: )in.-\n") or "") .. err end 
-		geterrorhandler()(err)
-	end
-
-	-- LibStub:FinalizeLibrary(major, [callback])
-	--
-	-- major (string) - The major version of the library 
-	-- callback (function)  - A function to be called when a new library is loaded.
-	-- If this function returns a true value, then after being called, it will be removed from the callback registry.
-	function LibStub:FinalizeLibrary(major, callback)
-		assert(type(major) == "string", ("Bad argument #2 to 'GetLibrary' (string expected, got %s)"):format(type(major)))
-		assert(not callback or type(callback) == "function", ("Bad argument #3 to 'FinalizeLibrary' (function or nil expected, got %s)"):format(type(callback)))
-	
-		local entry = assert(self.libs[major], ("Cannot finalize an unregistered instance of  %s."):format(major))
-	
-		entry.callback = callback
-	
-		for key, lib in pairs(self.libs) do
-			if lib ~= entry and type(lib.callback) == "function" then
-				if safecall(lib.callback, major, entry.instance) then lib.callback = nil end
-			end
-		end
-	end
-	
-	-- LibStub:IterateLibrary()
-	-- 
-	-- Returns an iterator for the currently registered libraries
-	function LibStub:IterateLibraries() return pairs(self.libs) end
-	
+    -- LibStub:IterateLibraries()
+    -- 
+    -- Returns an iterator for the currently registered libraries
+    function LibStub:IterateLibraries() 
+        return pairs(self.libs) 
+    end
+    
 	LibStub.minor = LIBSTUB_MINOR
-	getmetatable(LibStub).__call = LibStub.GetLibrary
+	setmetatable(LibStub, { __call = LibStub.GetLibrary })
 end
