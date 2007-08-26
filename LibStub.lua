660ed08a27ed877b59c7427cc4e838422d594c21
ckknight <ckknight@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-26 09:04:32 +0000

    LibStub - small optimization.
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@53 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -31,7 +31,7 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- throws an error if the library can not be found
 	-- returns the library object if found
 	function LibStub:GetLibrary(major, silent)
-		if not silent and not self.libs[major or "nil"] then error(("Cannot find a library instance of '%s'."):format(tostring(major)), 2) end
+		if not silent and not self.libs[major] then error(("Cannot find a library instance of '%s'."):format(tostring(major)), 2) end
 		return self.libs[major]
 	end
 	
