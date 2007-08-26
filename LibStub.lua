d41ded7bea25848b0fafb82f619fe7e7ed308167
ammo <ammo@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-26 06:58:24 +0000

    LibStub: LS - spaces -> tabs
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@46 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -18,7 +18,7 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- returns nil if a newer or same version of the lib is already present
 	-- returns empty library object or old library object if upgrade is needed
 	function LibStub:NewLibrary(major, minor)
-        minor = tonumber(minor) or assert(tonumber(minor:match("%d+")), "Minor version must either be a number or contain a number.")
+		minor = tonumber(minor) or assert(tonumber(minor:match("%d+")), "Minor version must either be a number or contain a number.")
 	
 		local entry = self.libs[major] or {}
 	
@@ -37,11 +37,11 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- throws an error if the library can not be found
 	-- returns the library object if found
 	function LibStub:GetLibrary(major, silent)
-        assert(type(major) == "string", ("Bad argument #2 to 'GetLibrary' (string expected, got %s)"):format(type(major)))
+		assert(type(major) == "string", ("Bad argument #2 to 'GetLibrary' (string expected, got %s)"):format(type(major)))
 	
 		local entry = silent and self.libs[major] or assert(self.libs[major], ("Cannot find a library instance of %s."):format(major))
 	
-        return entry and entry.instance or nil
+		return entry and entry.instance or nil
 	end
 
 	local function safecall(func,...)
@@ -59,7 +59,7 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- If this function returns a true value, then after being called, it will be removed from the callback registry.
 	function LibStub:FinalizeLibrary(major, callback)
 		assert(type(major) == "string", ("Bad argument #2 to 'GetLibrary' (string expected, got %s)"):format(type(major)))
-        assert(not callback or type(callback) == "function", ("Bad argument #3 to 'FinalizeLibrary' (function or nil expected, got %s)"):format(type(callback)))
+		assert(not callback or type(callback) == "function", ("Bad argument #3 to 'FinalizeLibrary' (function or nil expected, got %s)"):format(type(callback)))
 	
 		local entry = assert(self.libs[major], ("Cannot finalize an unregistered instance of  %s."):format(major))
 	
@@ -72,11 +72,11 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 		end
 	end
 	
-    -- LibStub:IterateLibrary()
-    -- 
-    -- Returns an iterator for the currently registered libraries
-    function LibStub:IterateLibraries() return pairs(self.libs) end
-    
+	-- LibStub:IterateLibrary()
+	-- 
+	-- Returns an iterator for the currently registered libraries
+	function LibStub:IterateLibraries() return pairs(self.libs) end
+	
 	LibStub.minor = LIBSTUB_MINOR
 	getmetatable(LibStub).__call = LibStub.GetLibrary
 end
