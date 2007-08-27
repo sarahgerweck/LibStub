dcbc2de3294fec951c02dc279f29f09282597ca8
ckknight <ckknight@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-27 08:03:35 +0000

    LibStub - make test more comprehensive, test creation, updating.
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@64 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- tests/test.lua
+++ tests/test.lua
@@ -3,4 +3,26 @@ strmatch = string.match
 
 loadfile("../LibStub.lua")()
 
-local Pants = LibStub:NewLibrary("Pants", 1)
+local lib, oldMinor = LibStub:NewLibrary("Pants", 1)
+assert(lib)
+assert(not oldMinor)
+
+function lib:MyMethod()
+end
+local MyMethod = lib.MyMethod
+lib.MyTable = {}
+local MyTable = lib.MyTable
+
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 1) -- check equal
+assert(not newLib)
+
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 0) -- check less
+assert(not newLib)
+
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 2)
+assert(newLib)
+assert(newLib == lib)
+assert(newOldMinor == 1)
+
+assert(lib.MyMethod == MyMethod) -- verify that values were saved
+assert(lib.MyTable == MyTable) -- verify that values were saved
