9f6dbe968d92d57a27cabb644a2fbac63773f3b6
ckknight <ckknight@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-27 05:46:05 +0000

    LibStub - fix a typo
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@61 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -20,7 +20,7 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 		minor = assert(tonumber(strmatch(minor, "%d+")), "Minor version must either be a number or contain a number.")
 		
 		if self.minors[major] and self.minors[major] >= minor then return nil end
-        local oldminor = self.minors[major[
+        local oldminor = self.minors[major]
 		self.minors[major], self.libs[major] = minor, self.libs[major] or {}
 		return self.libs[major], oldminor
 	end
