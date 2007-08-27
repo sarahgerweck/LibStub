91c1cdc14e6a10f1f36f7c72b6883719128e85d6
ckknight <ckknight@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-27 08:35:36 +0000

    LibStub (tests) - add comments to test.lua and test2.lua, also change some =='s into rawequal's.
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@68 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- tests/test.lua
+++ tests/test.lua
@@ -3,38 +3,39 @@ strmatch = string.match
 
 loadfile("../LibStub.lua")()
 
-local lib, oldMinor = LibStub:NewLibrary("Pants", 1)
-assert(lib)
-assert(not oldMinor)
+local lib, oldMinor = LibStub:NewLibrary("Pants", 1) -- make a new thingy
+assert(lib) -- should return the library table
+assert(not oldMinor) -- should not return the old minor, since it didn't exist
 
+-- the following is to create data and then be able to check if the same data exists after the fact
 function lib:MyMethod()
 end
 local MyMethod = lib.MyMethod
 lib.MyTable = {}
 local MyTable = lib.MyTable
 
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", 1) -- check equal
-assert(not newLib)
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 1) -- try to register a library with the same version, should silently fail
+assert(not newLib) -- should not return since out of date
 
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", 0) -- check less
-assert(not newLib)
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 0) -- try to register a library with a previous, should silently fail
+assert(not newLib) -- should not return since out of date
 
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", 2)
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 2) -- register a new version
+assert(newLib) -- library table
+assert(rawequal(newLib, lib)) -- should be the same reference as the previous
+assert(newOldMinor == 1) -- should return the minor version of the previous version
+
+assert(rawequal(lib.MyMethod, MyMethod)) -- verify that values were saved
+assert(rawequal(lib.MyTable, MyTable)) -- verify that values were saved
+
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", "Blah 3 Blah") -- register a new version with a string minor version (instead of a number)
+assert(newLib) -- library table
+assert(newOldMinor == 2) -- previous version was 2
+
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", "Blah 4 Blah") -- register a new version with a string minor version (instead of a number)
 assert(newLib)
-assert(newLib == lib)
-assert(newOldMinor == 1)
+assert(newOldMinor == 3) -- previous version was 3 (even though it gave a string)
 
-assert(lib.MyMethod == MyMethod) -- verify that values were saved
-assert(lib.MyTable == MyTable) -- verify that values were saved
-
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", "Blah 3 Blah")
+local newLib, newOldMinor = LibStub:NewLibrary("Pants", 5) -- register a new library, using a normal number instead of a string
 assert(newLib)
-assert(newOldMinor == 2)
-
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", "Blah 4 Blah")
-assert(newLib)
-assert(newOldMinor == 3)
-
-local newLib, newOldMinor = LibStub:NewLibrary("Pants", 5)
-assert(newLib)
-assert(newOldMinor == 4)
+assert(newOldMinor == 4) -- previous version was 4 (even though it gave a string)
--- tests/test2.lua
+++ tests/test2.lua
@@ -3,22 +3,24 @@ strmatch = string.match
 
 loadfile("../LibStub.lua")()
 
-for major in LibStub:IterateLibraries() do
+for major, library in LibStub:IterateLibraries() do
+	-- check that MyLib doesn't exist yet, by iterating through all the libraries
 	assert(major ~= "MyLib")
 end
 
-assert(not LibStub:GetLibrary("MyLib", true))
-assert(not pcall(LibStub.GetLibrary, LibStub, "MyLib")) -- should error
-local lib = LibStub:NewLibrary("MyLib", 1)
-assert(lib)
-assert(LibStub:GetLibrary("MyLib") == lib)
+assert(not LibStub:GetLibrary("MyLib", true)) -- check that MyLib doesn't exist yet by direct checking
+assert(not pcall(LibStub.GetLibrary, LibStub, "MyLib")) -- don't silently fail, thus it should raise an error.
+local lib = LibStub:NewLibrary("MyLib", 1) -- create the lib
+assert(lib) -- check it exists
+assert(rawequal(LibStub:GetLibrary("MyLib"), lib)) -- verify that :GetLibrary("MyLib") properly equals the lib reference
 
 local good = false
 for major, library in LibStub:IterateLibraries() do
-	if major == "MyLib" then
-		good = true
-		assert(library == lib)
-		break
+	-- check that MyLib exists somewhere in the libraries, by iterating through all the libraries
+	if major == "MyLib" then -- we found it!
+		good = true -- so that we know it exists
+		assert(rawequal(library, lib)) -- verify that the references are equal
+		break -- no point in continuing
 	end
 end
-assert(good)
+assert(good) -- verify that we actually found it
