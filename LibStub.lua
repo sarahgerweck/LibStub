5f00641216b1f87b517bd84c20478c6a417d669a
joshborke <joshborke@362765ee-ab4b-4ba6-a69f-d313b1a24502>
2007-08-29 22:46:23 +0000

    LibStub: LS-8 - minor optimization to GetLibrary (<3 shadowed :P)
    git-svn-id: svn://svn.wowace.com/wow/libstub/mainline/tmp/trunk/LibStub@74 362765ee-ab4b-4ba6-a69f-d313b1a24502

--- LibStub.lua
+++ LibStub.lua
@@ -33,7 +33,7 @@ if not LibStub or LibStub.minor < LIBSTUB_MINOR then
 	-- throws an error if the library can not be found (except silent is set)
 	-- returns the library object if found
 	function LibStub:GetLibrary(major, silent)
-		if not silent and not self.libs[major] then
+		if not self.libs[major] and not silent then
 			error(("Cannot find a library instance of %q."):format(tostring(major)), 2)
 		end
 		return self.libs[major], self.minors[major]
